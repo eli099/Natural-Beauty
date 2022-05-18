6b317ac138c42cb3bf783be8d3eefedcac9ec7c3
fransurf <flcknight@gmail.com>
2022-05-18 11:45:12 +0100

    more data Fran

--- db/data/parksRec.js
+++ db/data/parksRec.js
@@ -85,7 +85,7 @@ const parksRec = [
         link: 'https://www.alltrails.com/trail/england/devon/valley-of-the-rocks-and-lynton-outer-loop'
       }
     ],
-    activities: ['Walking', 'Hiking', 'Cycling', 'Kayaking', 'Paddleboarding', 'Foraging', 'Stargazing'],
+    activities: ['Walking', 'Hiking', 'Cycling', 'Horse-riding', 'Wild Swmming', 'Sea-Kayaking', 'Stargazing'],
     reviews: {
       beauty: 5,
       wildlife: 3,
@@ -96,13 +96,13 @@ const parksRec = [
     usersSay: [SELECT],
     attractions: [
       {
-        name: 'Stargazing at SDNP International Dark Skies Reserve',
-        code: 'IDSR',
-        location: [50.977683, -0.980305],
-        description: 'As one of 10 Dark Skies Discovery Sites within the National Park, Seven Sisters Country Park is one of the best places to view the night sky. On a clear night from October through to March, it\'s possible to catch glimpses of the Milky Way as it rises above the famous chalk cliffs.',
-        keyFeature: 'Stargazing',
-        localImg: ['../../assets/parkImgs/SDNP/IDSR1.jpeg', '../../assets/parkImgs/SDNP/IDSR2.jpeg', '../../assets/parkImgs/SDNP/IDSR3.jpeg'],
-        moreInfo: 'https://www.southdowns.gov.uk/dark-night-skies/where-to-stargaze/',
+        name: 'Lynton & Lynmouth Cliff Railway',
+        code: 'LCR',
+        location: [51.1353, -3.5004],
+        description: 'The Lynton and Lynmouth Cliff Railway is the highest and steepest water powered railway in the world. Who would have thought it, quiet little Exmoor has it\'s very own World Number 1. If you\'re after a great spot for a view of the coast with a cream tea to finish, this will be hard to beat',
+        keyFeature: 'Spectacular views',
+        localImg: ['../../assets/parkImgs/EMNP/LCR1.jpeg', '../../assets/parkImgs/EMNP/LCR1.jpeg', '../../assets/parkImgs/EMNP/LCR1.jpeg'],
+        moreInfo: 'https://www.thebestofexmoor.co.uk/blog/lynton-lynmouth-cliff-railway/',
         usersSay: [SELECT]
       },
       {
@@ -116,12 +116,76 @@ const parksRec = [
         usersSay: [SELECT]
       },
       {
-        name: 'Arundel Wetland Centre',
-        code: 'AWC',
+        name: 'Wild-swmming at Watersmeet',
+        code: 'WSW',
         location: [50.864231, -0.550414],
-        description: 'A gorgeous nature reserve, home to incredible wetland birds. With wide-open spaces and wetlands full of the sights and sounds of nature, there\'s plenty to see and do at Arundel Wetland Centre.',
-        keyFeature: 'Bird-watching',
-        localImg: ['../../assets/parkImgs/SDNP/AWC1.jpeg', '../../assets/parkImgs/SDNP/AWC2.jpeg', '../../assets/parkImgs/SDNP/AWC4.jpeg'],
+        description: 'The East Lyn River and Hoar Oak Water come together at Watersmeet to create a glorious waterfall. It\'s a fantastic spot for wild swimming, with small plunge pools by the waterfall, or, a little further along the river from there, you will find Long Pool which stretches further and goes deeper…',
+        keyFeature: 'Wild-swmming',
+        localImg: ['../../assets/parkImgs/EMNP/WSW1.jpeg', '../../assets/parkImgs/EMNP/WSW2.jpeg', '../../assets/parkImgs/SDNP/EMNP/WSW3.jpeg'],
+        moreInfo: 'https://www.visitarundel.co.uk/listing/arundel-wetland-centre/',
+        usersSay: [SELECT]
+      }
+    ]
+  },
+  {
+    code: '',
+    name: ' National Park',
+    location: [51.1346, -3.6462],
+    postcode: '',
+    description: '',
+    parkImg: ['../../assets/parkImgs/EMNP/EMNP1.jpeg', '../../assets/parkImgs/EMNP/EMNP1.jpeg', '../../assets/parkImgs/EMNP/EMNP1.png'],
+    website: 'https://www.exmoor-nationalpark.gov.uk/', 
+    iconicWildlife: ['wild red deer', 'Exmoor ponies', 'otters', 'butterflies', ],
+    keyWildlife: ['wild red deer'],
+    trails: [{
+      name: 'The South West Coastal Path',
+      link: 'https://www.southwestcoastpath.org.uk/walk-coast-path/south-west-coast-path-national-trail/day-walks/#'}, 
+      {
+        name: 'The Tarr Steps',
+        link: 'https://www.thebestofexmoor.co.uk/blog/tarr-steps/'
+      },
+      {
+        name: 'Valley of the Rocks',
+        link: 'https://www.alltrails.com/trail/england/devon/valley-of-the-rocks-and-lynton-outer-loop'
+      }
+    ],
+    activities: ['Walking', 'Hiking', 'Cycling', 'Horse-riding', 'Wild Swmming', 'Sea-Kayaking', 'Stargazing'],
+    reviews: {
+      beauty: 5,
+      wildlife: 3,
+      trails: 5,
+      camping: 2,
+      // foodDrink: 4,
+    },
+    usersSay: [SELECT],
+    attractions: [
+      {
+        name: 'Lynton & Lynmouth Cliff Railway',
+        code: 'LCR',
+        location: [51.1353, -3.5004],
+        description: 'The Lynton and Lynmouth Cliff Railway is the highest and steepest water powered railway in the world. Who would have thought it, quiet little Exmoor has it\'s very own World Number 1. If you\'re after a great spot for a view of the coast with a cream tea to finish, this will be hard to beat',
+        keyFeature: 'Spectacular views',
+        localImg: ['../../assets/parkImgs/EMNP/LCR1.jpeg', '../../assets/parkImgs/EMNP/LCR1.jpeg', '../../assets/parkImgs/EMNP/LCR1.jpeg'],
+        moreInfo: 'https://www.thebestofexmoor.co.uk/blog/lynton-lynmouth-cliff-railway/',
+        usersSay: [SELECT]
+      },
+      {
+        name: 'Visit Dunster Castle',
+        code: 'DC',
+        location: [51.1859, -3.4389],
+        description: 'Dunster Castle is steeped in history, well yes, it\'s a castle, but this building is so wonderfully preserved with the efforts of the National Trust, keeping this castle authentic and true to history. Great views of castle on approach, sitting majestically high, with Fabulous gardens, fascinating house and relaxed atmosphere.',
+        keyFeature: 'Historically preserved castle',
+        localImg: ['../../assets/parkImgs/EMNP/DC1.jpeg', '../../assets/parkImgs/EMNP/DC2.jpeg', '../../assets/parkImgs/EMNP/DC3.jpeg'],
+        moreInfo: 'https://www.thebestofexmoor.co.uk/blog/dunster-castle/',
+        usersSay: [SELECT]
+      },
+      {
+        name: 'Wild-swmming at Watersmeet',
+        code: 'WSW',
+        location: [50.864231, -0.550414],
+        description: 'The East Lyn River and Hoar Oak Water come together at Watersmeet to create a glorious waterfall. It\'s a fantastic spot for wild swimming, with small plunge pools by the waterfall, or, a little further along the river from there, you will find Long Pool which stretches further and goes deeper…',
+        keyFeature: 'Wild-swmming',
+        localImg: ['../../assets/parkImgs/EMNP/WSW1.jpeg', '../../assets/parkImgs/EMNP/WSW2.jpeg', '../../assets/parkImgs/SDNP/EMNP/WSW3.jpeg'],
         moreInfo: 'https://www.visitarundel.co.uk/listing/arundel-wetland-centre/',
         usersSay: [SELECT]
       }
