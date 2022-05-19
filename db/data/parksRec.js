7f7229e709865e0b09f64bf6d5aa3adef2201d64
fransurf <flcknight@gmail.com>
2022-05-19 12:46:19 +0100

    parkSchema updated to match data

--- db/data/parksRec.js
+++ db/data/parksRec.js
@@ -8,10 +8,11 @@ const parksRec = [
     parkImg: ['../../assets/parkImgs/SDNP/SDNP1.png', '../../assets/parkImgs/SDNP/SDNP2.jpeg', '../../assets/parkImgs/SDNP/SDNP3.jpeg'],
     website: 'https://www.southdowns.gov.uk/', 
     iconicWildlife: ['burnt orchid', 'round-headed rampion', 'otter', 'skylark', 'barn owl', 'brown trout', 'barbastelle bat', 'chalk carpet moth', 'sundew (carnivorous plant)'],
-    keyWildlife: ['butterflies'],
+    keyWildlife: 'butterflies',
     trails: [{
       name: 'South Downs Way',
-      link: 'https://www.nationaltrail.co.uk/en_GB/trails/south-downs-way/'}, 
+      link: 'https://www.nationaltrail.co.uk/en_GB/trails/south-downs-way/'
+      }, 
       {
         name: 'Kingley Vale Trail',
         link: 'https://www.thegreatsussexway.org/things-to-do/kingley-vale-trail/'
@@ -35,6 +36,7 @@ const parksRec = [
         name: 'Stargazing at SDNP International Dark Skies Reserve',
         code: 'IDSR',
         location: [50.977683, -0.980305],
+        category: 'Wild adventure',
         description: 'As one of 10 Dark Skies Discovery Sites within the National Park, Seven Sisters Country Park is one of the best places to view the night sky. On a clear night from October through to March, it\'s possible to catch glimpses of the Milky Way as it rises above the famous chalk cliffs.',
         keyFeature: 'Stargazing',
         localImg: ['../../assets/parkImgs/SDNP/IDSR1.jpeg', '../../assets/parkImgs/SDNP/IDSR2.jpeg', '../../assets/parkImgs/SDNP/IDSR3.jpeg'],
@@ -45,6 +47,7 @@ const parksRec = [
         name: 'Seven Sisters Experience',
         code: 'SSE',
         location: [50.776146, 0.152066],
+        category: 'Spectacular scenery',
         description: 'Globally-important habitats, unique wildlife and heritage secrets make Seven Sisters a truly special place. Your route will take you through an area popular with smugglers, folklore and a diverse natural wildlife habitat ranging from forest, chalk downland and the seashore.',
         keyFeature: 'Spectacular coastal views',
         localImg: ['../../assets/parkImgs/SDNP/SSE1.jpeg', '../../assets/parkImgs/SDNP/SSE2.jpeg', '../../assets/parkImgs/SDNP/SSE3.jpeg'],
@@ -55,6 +58,7 @@ const parksRec = [
         name: 'Arundel Wetland Centre',
         code: 'AWC',
         location: [50.864231, -0.550414],
+        category: 'Hidden gem',
         description: 'A gorgeous nature reserve, home to incredible wetland birds. With wide-open spaces and wetlands full of the sights and sounds of nature, there\'s plenty to see and do at Arundel Wetland Centre.',
         keyFeature: 'Bird-watching',
         localImg: ['../../assets/parkImgs/SDNP/AWC1.jpeg', '../../assets/parkImgs/SDNP/AWC2.jpeg', '../../assets/parkImgs/SDNP/AWC4.jpeg'],
@@ -72,7 +76,7 @@ const parksRec = [
     parkImg: ['../../assets/parkImgs/EMNP/EMNP1.jpeg', '../../assets/parkImgs/EMNP/EMNP2.jpeg', '../../assets/parkImgs/EMNP/EMNP3.png'],
     website: 'https://www.exmoor-nationalpark.gov.uk/', 
     iconicWildlife: ['wild red deer', 'Exmoor ponies', 'otters', 'butterflies', ],
-    keyWildlife: ['wild red deer'],
+    keyWildlife: 'wild red deer',
     trails: [{
       name: 'The South West Coastal Path',
       link: 'https://www.southwestcoastpath.org.uk/walk-coast-path/south-west-coast-path-national-trail/day-walks/#'
@@ -100,6 +104,7 @@ const parksRec = [
         name: 'Lynton & Lynmouth Cliff Railway',
         code: 'LCR',
         location: [51.1353, -3.5004],
+        category: 'Hidden gem',
         description: 'The Lynton and Lynmouth Cliff Railway is the highest and steepest water powered railway in the world. Who would have thought it, quiet little Exmoor has it\'s very own World Number 1. If you\'re after a great spot for a view of the coast with a cream tea to finish, this will be hard to beat',
         keyFeature: 'Spectacular views',
         localImg: ['../../assets/parkImgs/EMNP/LCR1.jpeg', '../../assets/parkImgs/EMNP/LCR2.jpeg', '../../assets/parkImgs/EMNP/LCR3.jpeg'],
@@ -110,6 +115,7 @@ const parksRec = [
         name: 'Visit Dunster Castle',
         code: 'DC',
         location: [51.1859, -3.4389],
+        category: 'Spectacular scenery',
         description: 'Dunster Castle is steeped in history, well yes, it\'s a castle, but this building is so wonderfully preserved with the efforts of the National Trust, keeping this castle authentic and true to history. Great views of castle on approach, sitting majestically high, with Fabulous gardens, fascinating house and relaxed atmosphere.',
         keyFeature: 'Historically preserved castle',
         localImg: ['../../assets/parkImgs/EMNP/DC1.jpeg', '../../assets/parkImgs/EMNP/DC2.jpeg', '../../assets/parkImgs/EMNP/DC3.jpeg'],
@@ -117,9 +123,10 @@ const parksRec = [
         // usersSay: [SELECT]
       },
       {
-        name: 'Wild-swmming at Watersmeet',
+        name: 'Wild-swimming at Watersmeet',
         code: 'WSW',
         location: [50.864231, -0.550414],
+        category: 'Wild adventure',
         description: 'The East Lyn River and Hoar Oak Water come together at Watersmeet to create a glorious waterfall. It\'s a fantastic spot for wild swimming, with small plunge pools by the waterfall, or, a little further along the river from there, you will find Long Pool which stretches further and goes deeper…',
         keyFeature: 'Wild-swmming',
         localImg: ['../../assets/parkImgs/EMNP/WSW1.jpeg', '../../assets/parkImgs/EMNP/WSW2.jpeg', '../../assets/parkImgs/EMNP/WSW3.jpeg'],
@@ -137,7 +144,7 @@ const parksRec = [
     parkImg: ['../../assets/parkImgs/YDNP/YDNP1.jpeg', '../../assets/parkImgs/YDNP/YDNP2.jpeg', '../../assets/parkImgs/YDNP/YDNP3.png'],
     website: 'https://www.yorkshiredales.org.uk/', 
     iconicWildlife: ['curlews', 'lapwings', 'black grouse', 'skylarks', 'peregrine falcons','red squirrels'],
-    keyWildlife: ['peregrine falcons'],
+    keyWildlife: 'peregrine falcons',
     trails: [{
       name: 'Yorkshire Three Peak Challenge',
       link: 'https://www.threepeakschallenge.uk/yorkshire-three-peaks-challenge/'
@@ -165,6 +172,7 @@ const parksRec = [
         name: 'The 3 Peaks Challenge',
         code: '3PC',
         location: [54.227730, -2.402498],
+        category: 'Wild adventure',
         description: 'The Yorkshire Dales has some of the most spectacular peaks in England, and the famous Yorkshire Three Peaks of Whernside, Ingleborough and Pen-y-Ghent are amongst the highest in the county, providing an inspiring challenge to walkers from around the world.',
         keyFeature: 'Hiking',
         localImg: ['../../assets/parkImgs/YDNP/3PC1.jpeg', '../../assets/parkImgs/YDNP/3PC2.jpeg', '../../assets/parkImgs/YDNP/3PC3.png'],
@@ -175,6 +183,7 @@ const parksRec = [
         name: 'Stump Cross Caverns',
         code: 'SCC',
         location: [54.067506, -2.0054333],
+        category: 'Spectacular scenery',
         description: 'An ancient natural cave formation deep underneath the Yorkshire Dales',
         keyFeature: 'Underground caves',
         localImg: ['../../assets/parkImgs/YDNP/SCC1.jpeg', '../../assets/parkImgs/YDNP/SCC2.jpeg', '../../assets/parkImgs/YDNP/SCC3.jpeg'],
@@ -185,6 +194,7 @@ const parksRec = [
         name: 'Jump in at Loup Scar',
         code: 'JLC',
         location: [54.3695, -1.572196],
+        category: 'Hidden gem',
         description: 'A fantastic stretch of grassy riverside incorporating river, meadow and beaches, with the limestone cliffs and the gorges of Loup Scar. There\'s a terrifying jump at Loup Scar, from the path on river R, into a small very deep plunge pool.',
         keyFeature: 'Wild-swmming',
         localImg: ['../../assets/parkImgs/YDNP/JLC1.jpeg', '../../assets/parkImgs/YDNP/JLC2.jpeg', '../../assets/parkImgs/SDNP/YDNP/JLC3.jpeg'],
@@ -202,7 +212,7 @@ const parksRec = [
     parkImg: ['../../assets/parkImgs/PDNP/PDNP1.jpeg', '../../assets/parkImgs/PDNP/PDNP2.jpeg', '../../assets/parkImgs/PDNP/PDNP3.jpeg'],
     website: 'https://www.peakdistrict.gov.uk/', 
     iconicWildlife: ['wild red deer', 'owls', 'curlews', 'sparrowhawks', 'buzzards'],
-    keyWildlife: ['wild red deer'],
+    keyWildlife: 'wild red deer',
     trails: [{
       name: 'The Kinder moorland walk',
       link: 'https://www.nationaltrust.org.uk/kinder-edale-and-the-dark-peak/trails/kinder-moorland-walk---a-glimpse-into-the-future'
@@ -270,7 +280,7 @@ const parksRec = [
     parkImg: ['../../assets/parkImgs/DMNP/DMNP1.jpeg', '../../assets/parkImgs/DMNP/DMNP2.jpeg', '../../assets/parkImgs/DMNP/DMNP3.jpeg'],
     website: 'https://www.dartmoor.gov.uk/', 
     iconicWildlife: ['blue ground beetle', 'cuckoo', 'marsh fritillary butterfly', 'otter', 'ash black slug', 'hazel dormouse', 'rare bats'],
-    keyWildlife: ['butterflies'],
+    keyWildlife: 'butterflies',
     trails: [{
       name: 'Buckfastleigh Walk',
       link: 'https://www.dartmoor.gov.uk/enjoy-dartmoor/outdoor-activities/walking/walking-routes/welcome-to-dartmoor-buckfastleigh-walk'
--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const parkSchema = new mongoose.Schema({
-  code: { type: Number, required: false },
+  code: { type: String, required: false },
   name: { type: String, required: true },
   location: { type: Array, required: false },
   postcode: { type: String, required: false },
@@ -9,11 +9,23 @@ const parkSchema = new mongoose.Schema({
   parkImg: { type: Array, required: false },
   website: { type: String, required: true },
   iconicWildlife: { type: Array, required: false },
-  keyWildlife: { type: Array, required: false },
-  trails: { type: Array, required: false },
+  keyWildlife: { type: String, required: false },
+  trails: [{
+    name: { type: String, required: false },
+    link: { type: String, required: false }
+  }],
   activities: { type: Array, required: false },
   reviews: { type: Object, required: false },
-  attractions: { type: Array, required: false },
+  attractions: [{
+    code: { type: String, required: false },
+    name: { type: String, required: true },
+    location: { type: Array, required: false },
+    category: { type: String, required: false },
+    description: { type: String, required: false },
+    keyFeature: { type: String, required: false },
+    localImg: { type: Array, required: false },
+    moreInfo: { type: String, required: false }
+  }]
 })
 
 export default mongoose.model('Park', parkSchema)
