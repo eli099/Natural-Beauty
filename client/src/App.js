9cf5be5ebf7ef28c5697f4d2ecf724924ef6ea77
Ben Cave <bencaveuk@gmail.com>
2022-05-20 17:13:46 +0100

    fri afternoon

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,10 @@ import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Home from './Home'
 import Map from './components/Map'
-<<<<<<< HEAD
 import ParksIndex from './components/parks/ParksIndex'
-=======
 import Park from './components/parks/Park'
 import NavBarPage from './components/common/NavBarPage'
 import ParkMap from './components/maps/ParkMap'
->>>>>>> development
 
 const App = () => {
 
@@ -42,18 +39,12 @@ const App = () => {
           {/* Auth routes */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
-          <Route path="/maptest" element={<Map />}></Route>
-
           <Route path="/parks" element={<ParksIndex />} />
-=======
-
           {/* Maps */}
           <Route path="/maptest" element={<Map />} />
-          <Route path="/park" element={<Park />} />
+          {/* <Route path="/park" element={<Park />} /> */}
           <Route path="/maptest" element={<Map />} />
           <Route path="/parkmaptest" element={<ParkMap />} />
->>>>>>> development
         </Routes>
       </BrowserRouter>
     </main>
--- client/src/components/maps/ParkMap.js
+++ client/src/components/maps/ParkMap.js
@@ -1,20 +1,42 @@
 import { useRef, useEffect, useState } from 'react'
 
 import mapboxgl from 'mapbox-gl'
+import axios from 'axios'
+
+import { useParams } from 'react-router-dom'
 
 const ParkMap = () => {  
 
   mapboxgl.accessToken = 'pk.eyJ1IjoibXJicmVhZCIsImEiOiJjbDM4bHV0Z3UwMTRmM2tueTY1Mm41NTZnIn0.92r4wGEn7bywx1dmpYCe-w'
 
+  const [ park, setPark ] = useState(null)
+  const [ errors, setErrors ] = useState(false)
   const parkMapContainer = useRef(null);
   const map = useRef(null);
+  const [ mapObject, setMapObject ] = useState(null)
   // eslint-disable-next-line
   const [lng, setLng] = useState(-3.9323); //set these to coordinates of centre of park (will have to calculate)
   // eslint-disable-next-line
   const [lat, setLat] = useState(54.6165); //same
   // eslint-disable-next-line
   const [zoom, setZoom] = useState(4.6); // set to zoom level so that park fills map frame (will have to calculate)
+  const { id } = useParams()
 
+
+  useEffect(() => {
+    const getPark = async () => {
+      try {
+        const { data } = await axios.get(`/api/parks/${id}`)
+        console.log('🏆 Got the Park data')
+        setPark(data)
+      } catch (error) {
+        console.log('error getting Park data ->', error)
+        setErrors(true)
+      }
+    }
+    getPark()
+  }, [id])
+  
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -23,7 +45,16 @@ const ParkMap = () => {
       center: [lng, lat],
       zoom: zoom
     })
-  })
+    setMapObject(map.current)
+  },[lat, lng, zoom])
+
+
+  // const changeCoords = () => {
+  //   mapObject.setLat(park.location[1])
+  //   mapObject.setLng(park.location[0])
+  //   // mapObject.setZoom(data.location[2])
+  //   mapObject.setZoom(7)
+  // }
 
   useEffect(() => {
     if(!map.current) return
@@ -66,13 +97,47 @@ const ParkMap = () => {
             'line-width': 2
         }
       })
+      map.current.addLayer({
+        'id' : 'park-centres',
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': [
+            {
+              'type': 'Feature',
+              'properties': {},
+              'geometry': {
+              'type': 'Point',
+              'coordinates': [ park.location[1], park.location[0] ]
+              }
+            },
+            // {
+            //   'type': 'Feature',
+            //   'properties': {},
+            //   'geometry': {
+            //   'type': 'Point',
+            //   'coordinates': [-90.3295, -0.6344]
+            // }
+            // },
+            // {
+            //   'type': 'Feature',
+            //   'properties': {},
+            //   'geometry': {
+            //   'type': 'Point',
+            //   'coordinates': [-91.3403, 0.0164]
+            // }
+            // }
+          ]
+        }
+      })
     })
-  })
+  }, [park])
 
   useEffect(() => {
     if(!map.current) return
     let hoveredStateId = null
     map.current.on('mousemove', 'national-parks', (e) => {
+      map.current.style.cursor = 'pointer'
       if (e.features.length > 0) {
           if (hoveredStateId !== null) {
               map.current.setFeatureState(
@@ -95,6 +160,15 @@ const ParkMap = () => {
             )
         }
         hoveredStateId = null
+    })
+  })
+
+  useEffect(() => {
+    if(!map.current) return
+    map.current.on('click', 'national-parks', (e) => {
+      map.current.flyTo({
+        center: e.features[0].geometry.coordinates
+      })
     })
   })
 
